c67d4c07e0accd02cc42b7ab3b0d06f87ec18477
GetAJobOneDay <72264962+GetAJobOneDay@users.noreply.github.com>
2020-11-02 00:05:39 +0700

    Add files via upload

---
+++ lab5/src/main/java/controller/DrawingLoop.java
@@ -0,0 +1,70 @@
+package controller;
+
+import model.Character;
+import view.Platform;
+
+import java.util.ArrayList;
+
+public class DrawingLoop implements Runnable {
+    private Platform platform;
+    private int frameRate;
+    private float interval;
+    private boolean running;
+    public DrawingLoop(Platform platform){
+        this.platform=platform;
+        frameRate =60;
+        interval=1000.0f/frameRate;
+        running=true;
+    }
+    public void checkCollision(ArrayList<Character> character) throws InterruptedException {
+        for(Character cha : character){
+            cha.checkReachGameWall();
+            cha.checkReachHighest();
+            cha.checkReachFloor();
+        }
+        for(Character cha : character){
+            for(Character cha2:character){
+                if(cha!=cha2){
+                    if(cha.getBoundsInParent().intersects(cha2.getBoundsInParent())){
+                        cha.collided(cha2);
+                        cha2.collided(cha);
+                    }
+                }
+            }
+        }
+    }
+
+    public void paint(ArrayList<Character> character){
+        for(Character cha : character){
+            cha.repaint();
+        }
+
+    }
+    @Override
+    public void run() {
+        System.out.println("thread drawing");
+        while (running){
+            float time = System.currentTimeMillis();
+            try {
+                checkCollision(platform.getCharacter());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            paint(platform.getCharacter());
+            time =System.currentTimeMillis()-time;
+            if(time<interval){
+                try{
+                    Thread.sleep((long) (interval-time));
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }else{
+                try{
+                    Thread.sleep((long)(interval-(interval%time)));
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ lab5/src/main/java/model/Character.java
@@ -0,0 +1,229 @@
+package model;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import view.Platform;
+import java.util.concurrent.TimeUnit;
+
+public class Character extends Pane {
+    private int score=0;
+    Logger logger = LoggerFactory.getLogger(Character.class);
+    public static final int CHARACTER_WIDTH = 32;
+    public static final int CHARACTER_HEIGHT = 64;
+    int yVelocity ;
+    int xVelocity ;
+    int maxa;
+    int maxy;
+    int maxVeloX;
+    int maxVeloY ;
+    boolean isMoveLeft = false;
+    boolean isMoveRight = false;
+    boolean isFalling = true;
+    private Image CharacterImg;
+    boolean canJump = false;
+    boolean isJumping = false;
+    private int x;
+    private int y;
+    private AnimatedSprite imageView;
+    private KeyCode leftKey, rightKey, upKey;
+    private int startX;
+    private int startY;
+    private int offsetX;
+    private int offsetY;
+    public boolean reachGameWall=false;
+    public Character(int x, int y, int offsetX, int offSetY, KeyCode lk, KeyCode rk, KeyCode uk,String name,int h, int w) {
+        this.startX=x;
+        this.startY=y;
+        this.offsetX=offsetX;
+        this.startY=offSetY;
+        this.xVelocity = 0;
+        this.yVelocity =0;
+        this.maxVeloX = 7;
+        this.maxVeloY =17;
+        this.maxa=1;
+        this.maxy=1;
+        this.x = x;
+        this.y = y;
+        this.setTranslateX(x);
+        this.setTranslateY(y);
+        this.CharacterImg = new Image(getClass().getResourceAsStream("/assets/"+name+".png"));
+        this.imageView = new AnimatedSprite(CharacterImg, 4, 4, 1, offsetX, offSetY, w, h);
+        this.leftKey = lk;
+        this.rightKey = rk;
+        this.upKey = uk;
+        this.getChildren().addAll(this.imageView);
+    }
+
+    public void moveX() {
+        setTranslateX(x);
+        if (isMoveLeft) {
+            xVelocity = xVelocity >= maxVeloX ? maxVeloX : xVelocity + maxa;
+            x = x - xVelocity;
+        }
+        if (isMoveRight) {
+            xVelocity = xVelocity >= maxVeloX ? maxVeloX : xVelocity + maxa;
+            x = x + xVelocity;
+        }
+    }
+
+    public void moveY() {
+        setTranslateY(y);
+        if (isFalling) {
+            yVelocity = yVelocity >= maxVeloY ? maxVeloY : yVelocity + maxy;
+            //if(y+yVelocity<=startY){
+                y = y + yVelocity;
+            //}
+        } else if (isJumping) {
+            yVelocity = xVelocity <= 0 ? 0 : yVelocity - maxy;
+            y = y - yVelocity;
+        }
+    }
+
+    public void checkReachFloor() {
+        if (isFalling && y >= Platform.GROUND - CHARACTER_HEIGHT) {
+            isFalling = false;
+            canJump = true;
+            yVelocity = 0;
+        }
+    }
+
+    public void repaint() {
+        moveY();
+        moveX();
+    }
+
+    public void jump() {
+        if (canJump) {
+            yVelocity = maxVeloY;
+            canJump = false;
+            isJumping = true;
+            isFalling = false;
+        }
+    }
+
+    public void checkReachHighest() {
+        if (isJumping && yVelocity <= 0) {
+            isJumping = false;
+            isFalling = true;
+            yVelocity = 0;
+        }
+    }
+
+    public KeyCode getLeftKey() {
+        return leftKey;
+    }
+
+    public KeyCode getRightKey() {
+        return rightKey;
+    }
+
+    public void moveLeft() {
+        isMoveLeft = true;
+        isMoveRight = false;
+    }
+
+    public void moveRight() {
+        isMoveLeft = false;
+        isMoveRight = true;
+    }
+
+    public void stop() {
+        isMoveRight = false;
+        isMoveLeft = false;
+    }
+
+    public void checkReachGameWall() {
+        if (x <= 0) {
+            x = 0;
+            this.reachGameWall=true;
+        } else if (x + CHARACTER_WIDTH >= Platform.WIDTH) {
+            this.reachGameWall=true;
+            x = Platform.WIDTH - CHARACTER_WIDTH;
+        }else{
+            this.reachGameWall=false;
+        }
+    }
+
+    public KeyCode getUpKey() {
+        return upKey;
+    }
+
+    public AnimatedSprite getImageView() {
+        return imageView;
+    }
+
+    public void trace() {
+        logger.info("x:{} y:{} vx:{} vy:{}", x, y, xVelocity, yVelocity);
+    }
+
+    public void collided(Character cha) throws InterruptedException {
+        System.out.println("adfdw");
+        if(isMoveLeft){
+            this.x = cha.getX()+CHARACTER_WIDTH;
+            stop();
+        }else if(isMoveRight){
+            this.x=cha.getX()-CHARACTER_WIDTH;
+            stop();
+        }
+        if(y<Platform.GROUND-CHARACTER_HEIGHT){
+            if(isFalling && y<cha.getY() && Math.abs(y-cha.getY())<=CHARACTER_HEIGHT){
+                score++;
+                y=Platform.GROUND-CHARACTER_HEIGHT-5;
+                repaint();
+                cha.collapsed();
+                cha.respwan();
+            }
+        }
+    }
+
+    private void collapsed() throws InterruptedException {
+        imageView.setFitHeight(5);
+        y=Platform.GROUND-5;
+        repaint();
+        TimeUnit.MILLISECONDS.sleep(500);
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public int getX() {
+        return Math.min(x, Platform.WIDTH - CHARACTER_WIDTH);
+    }
+
+    private void respwan() {
+        x=30;
+        y=30;
+        imageView.setFitWidth(CHARACTER_WIDTH);
+        imageView.setFitHeight(CHARACTER_HEIGHT);
+        isMoveLeft =false;
+        isMoveRight=false;
+        isFalling=true;
+        canJump=false;
+        isJumping = false;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public double getOffsetX() {
+        return this.offsetX;
+    }
+
+    public double getOffsetY() {
+        return this.offsetY;
+    }
+    public boolean isCanJump(){
+        return this.canJump;
+    }
+    public boolean isFalling(){
+        return this.isFalling;
+    }
+    public void setPosition(int x, int y){
+        this.startX = x;
+        this.startY = y;
+    }
+}
